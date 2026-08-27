d22746b95fc61f0905ba3f9dc812513f8cc53394
Peravali Mahendra <pmahendra9012@gmail.com>
2026-08-27 19:17:44 +0530

    Fix syntax error in aws_instance resource declaration

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "app_server" {
     Name = "Terraform_Demo"
   }
 }
-resource "aws_instance" web_server" {
+resource "aws_instance" "web_server" {
   ami           = "ami-0bdc7d025135d7b49"
   instance_type = "t3.micro"
   tags = {
