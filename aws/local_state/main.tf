bff5663ce2298f3b4249748eaf49556a8dd541df
Peravali Mahendra <pmahendra9012@gmail.com>
2026-08-27 18:43:28 +0530

    Change AWS region and update instance configuration

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,12 +10,12 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0bdc7d025135d7b49"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
